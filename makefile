6e53cfe82c13d72ca55414c731d57068dddf50df
Mohammad Amin Khakzadan <mak12776@gmail.com>
2019-08-14 18:23:18 +0430

    next commit

--- makefile
+++ makefile
@@ -2,3 +2,6 @@
 main: main.c
 	gcc -o $@ $<
 
+xcript: xcript.c misc.c error.c
+	gcc -o $@ $?
+
--- misc.h
+++ misc.h
@@ -2,9 +2,11 @@
 #ifndef MISC_H
 #define MISC_H 1
 
+#include <stdlib.h>
+#include <stdio.h>
+
 size_t fread_size(void *pntr, size_t size, FILE *file);
 size_t fwrite_size(void *pntr, size_t size, FILE *file);
-
 
 #endif 
 
